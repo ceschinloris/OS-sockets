78b966d399033313654ff5c34b49f005298dd196
SylvainRamseyer <sylvain.ramseyer@gmail.com>
2017-03-24 14:26:39 +0100

    dico de fonction

--- client.py
+++ client.py
@@ -5,15 +5,44 @@
 import os
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("", 1111))
-
-while not s._closed :
-    print("Le nom du fichier que vous voulez récupérer:")
-    file_name = input(">> ") # utilisez raw_input() pour les anciennes versions python
+def get(args):
+    file_name = args[1]
     s.send(file_name.encode())
     file_name = 'data/%s' % (os.path.basename(file_name),)
     r = s.recv(9999999)
     with open(file_name,'wb') as _file:
         _file.write(r)
     print("Le fichier a été correctement copié dans : %s." % file_name)
+
+
+def put(args):
+    pass
+
+    
+commands = {
+    "get":get,
+    "put":put,
+}
+
+
+print("enter the remote adress")
+address = input(">> ") # utilisez raw_input() pour les anciennes versions python
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((address, 1111))
+
+while not s._closed :
+    print("Le nom du fichier que vous voulez récupérer:")
+    command = input(">> ") # utilisez raw_input() pour les anciennes versions python
+
+    args = command.split(" ")
+
+    commands[args[0]](args)
+
+
+
+
+
+
+
+    
