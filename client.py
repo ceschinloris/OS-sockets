b74e2fa12357b244569ed9b64f3061768d0b339b
loris <ceschinlori@gmail.com>
2017-04-21 15:10:26 +0200

    cd + ls functions

--- client.py
+++ client.py
@@ -4,45 +4,111 @@
 # http://apprendre-python.com/page-reseaux-sockets-python-port
 import os
 import socket
+import pickle
 
-def get(args):
-    file_name = args[1]
-    s.send(file_name.encode())
-    file_name = 'data/%s' % (os.path.basename(file_name),)
-    r = s.recv(9999999)
-    with open(file_name,'wb') as _file:
-        _file.write(r)
-    print("Le fichier a été correctement copié dans : %s." % file_name)
+class Client:
 
 
-def put(args):
-    pass
+    CHIFFRE = 9999999
 
-    
-commands = {
-    "get":get,
-    "put":put,
-}
+    def __init__(self):
+        self.commands = {
+            "ls": self.ls,
+            "cd": self.cd,
+            "mkdir": self.mkdir,
+            "get": self.get,
+            "put": self.put,
+            "exec": self.exec,
+            "exit": self.exit,
+        }
 
+        # print("enter the remote adress")
+        # address = input(">> ")  # utilisez raw_input() pour les anciennes versions python
+        self.address = "127.0.0.1"
 
-print("enter the remote adress")
-address = input(">> ") # utilisez raw_input() pour les anciennes versions python
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((address, 1111))
-
-while not s._closed :
-    print("Le nom du fichier que vous voulez récupérer:")
-    command = input(">> ") # utilisez raw_input() pour les anciennes versions python
-
-    args = command.split(" ")
-
-    commands[args[0]](args)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((self.address, 1111))
+        self.current_path=''
+        self.get_current_path()
 
 
+    def ls(self, args):
+        """ args : void"""
+        self.s.send("ls:".encode())
+        r = self.s.recv(self.CHIFFRE)
+
+        r = pickle.loads(r)
+        print(r)
 
 
+    def cd(self, args):
+        """ args : 0 : path"""
+        path = args[0]
+        self.s.send(("cd:"+path).encode())
+        r = self.s.recv(self.CHIFFRE)
+
+        #error code
+
+        self.get_current_path()
 
 
+    def mkdir(self, args):
+        """ args : 0 : folder name"""
+        pass
 
-    
+
+    def get(self, args):
+        """ args : 0 : file name
+                   1 : save path"""
+        file_name = args[0]
+        self.s.send(("get:" + file_name).encode())
+        file_name = args[1] + '/' + (os.path.basename(file_name))
+        r = self.s.recv(self.CHIFFRE)
+        with open(file_name, 'wb') as _file:
+            _file.write(r)
+        print("File saved in " + file_name)
+
+
+    def put(self, args):
+        pass
+
+
+    def exec(self, args):
+        pass
+
+
+    def exit(self, args):
+        self.s.send(("exit:").encode())
+        self.s.close()
+
+    def get_current_path(self):
+        self.s.send("getpath:".encode())
+        r = self.s.recv(self.CHIFFRE)
+        self.current_path = r.decode()
+
+
+    def run(self):
+        while not self.s._closed:
+            # Faut input les méthodes
+            #
+            #   ls
+            #   cd
+            #   mkdir
+            #   get server_filename client_savepath
+            #   put client_filename server_savepath
+            #   run command (exec(command) sur le serveur)
+
+            command = input(self.current_path+">>")  # utilisez raw_input() pour les anciennes versions python
+
+            args = command.split(" ")
+
+            try:
+                self.commands[args[0]](args[1:])
+            except :
+                print("Invalid command")
+
+        print ("bye")
+
+if __name__ == '__main__':
+    c = Client()
+    c.run()
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import os
 import socket
 import threading
+import pickle
+from pathlib import Path
 
 
 class ClientThread(threading.Thread):
@@ -13,16 +15,71 @@ class ClientThread(threading.Thread):
         self.clientsocket = clientsocket
         print("[+] Nouveau thread pour %s %s" % (self.ip, self.port,))
 
+        self.is_running = True
+        self.commands = {
+            "ls": self.ls,
+            "cd": self.cd,
+            "mkdir": self.mkdir,
+            "get": self.get,
+            "put": self.put,
+            "exec": self.exec,
+            "exit": self.exit,
+            "getpath": self.get_current_path,
+        }
+        self.current_path = Path(os.getcwd())
+
     def run(self):
         print("Connection de %s %s" % (self.ip, self.port,))
-        while True:
-            r = self.clientsocket.recv(2048)
-            print("Ouverture du fichier: ", r, "...")
-            fp = open(r, 'rb')
-            self.clientsocket.send(fp.read())
-            # break si on veut quitter
+
+        while self.is_running:
+            command = self.clientsocket.recv(2048).decode()
+            if command is not '':
+                command_name, command_args = command.split(":", 1)
+
+                self.commands[command_name](command_args)
 
         print("Client déconnecté...")
+
+    def ls(self, args):
+        """ args : void"""
+        file_list = os.listdir(self.current_path.absolute())
+        self.clientsocket.send(pickle.dumps(file_list))
+
+
+    def cd(self, args):
+        """ args : 0 : path"""
+        if os.path.isdir(str(self.current_path)+'/'+args):
+
+            self.current_path = Path(os.path.join(self.current_path, args)).resolve()
+
+            self.clientsocket.send("1".encode())
+        else:
+            self.clientsocket.send("2".encode())
+
+
+    def mkdir(self, args):
+        """ args : 0 : folder name"""
+        pass
+
+    def get(self, args):
+        """ args : 0 : file name
+                   1 : save path"""
+
+        fp = open(args, 'rb')
+        self.clientsocket.send(fp.read())
+
+    def put(self, args):
+        pass
+
+    def exec(self, args):
+        pass
+
+    def exit(self, args):
+        self.is_running = False
+
+    def get_current_path(self,args):
+        """ args : void"""
+        self.clientsocket.send(str(self.current_path.absolute()).encode())
 
 
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
